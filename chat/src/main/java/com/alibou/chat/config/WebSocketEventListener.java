3b686c50b685e1ac8aa635225bee8e459b9fcc99
Danzo52-best <tintran2712@gmail.com>
2023-08-28 17:17:00 +0700

    completed code

---
+++ chat/src/main/java/com/alibou/chat/config/WebSocketEventListener.java
@@ -0,0 +1,33 @@
+package com.alibou.chat.config;
+
+import com.alibou.chat.chat.ChatMessage;
+import com.alibou.chat.chat.MessageType;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
+import org.springframework.messaging.simp.SimpMessageSendingOperations;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.messaging.SessionDisconnectEvent;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class WebSocketEventListener {
+
+    private final SimpMessageSendingOperations messageTemplate;
+    public void handleWebSocketDisconnectListener(SessionDisconnectEvent event){
+        //TO DO -- to be implemented
+        StompHeaderAccessor headerAccessor = StompHeaderAccessor.wrap(event.getMessage());
+        String username = (String) headerAccessor.getSessionAttributes().get("username");
+
+        if(username!=null){
+            log.info("User disconnected {}", username);
+            var chatMessage = ChatMessage.builder()
+                    .type(MessageType.LEAVE)
+                    .sender(username)
+                    .build();
+            messageTemplate.convertAndSend("/topic/public", chatMessage);
+        }
+    }
+}
